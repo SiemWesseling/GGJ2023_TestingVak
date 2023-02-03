af43a851dbdbe09ef422396a22d890452b19cf0f
Siem Wesseling <siemwesseling@gmail.com>
2023-02-03 21:37:02 +0100

    added melee attack for melee enemy

---
+++ Assets/Scripts/Characters/Enemy/EnemyMeleeAttacking.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMeleeAttacking : MonoBehaviour
+{
+    [SerializeField] private float damage;
+    [SerializeField] private float attackCoolDown;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
