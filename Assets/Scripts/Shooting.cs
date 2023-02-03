aee57c50b2f83d6866f5ad6a72b5771960871e6c
bossie911 <mikabos@live.nl>
2023-02-03 19:49:36 +0100

    added a cooldown for shooting

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -5,19 +5,33 @@ using UnityEngine;
 public class Shooting : MonoBehaviour
 {
     [SerializeField] private GameObject bullet;
-
-
+    private float shootingCooldown = 1f;
+    private float timer = 0;
+    private bool playerCanShoot = true;
     // Start is called before the first frame update
     void Start()
     {
-        
+        timer = shootingCooldown;
+    }
+
+    private void FixedUpdate()
+    {
+        //Timer
+        if (playerCanShoot == false)
+        {
+            timer += Time.deltaTime;
+            if (timer >= shootingCooldown)
+            {
+                playerCanShoot = true;
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         //Shooting
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && playerCanShoot)
         {
             Vector3 worldMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             Vector2 direction = (Vector2)((worldMousePos - transform.position));
@@ -29,6 +43,9 @@ public class Shooting : MonoBehaviour
             // Adds velocity to the bullet
             blt.GetComponent<Rigidbody2D>().velocity = direction * 2;
 
+            //Reset cooldown
+            playerCanShoot = false;
+            timer = 0;
         }
     }
 }
