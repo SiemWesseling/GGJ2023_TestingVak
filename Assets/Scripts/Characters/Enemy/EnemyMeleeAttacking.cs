31a953f715f2b9d6adb73667bfa7789cc07c50ff
Siem Wesseling <siemwesseling@gmail.com>
2023-02-03 22:17:10 +0100

    Enemy damages player

--- Assets/Scripts/Characters/Enemy/EnemyMeleeAttacking.cs
+++ Assets/Scripts/Characters/Enemy/EnemyMeleeAttacking.cs
@@ -7,13 +7,31 @@ public class EnemyMeleeAttacking : MonoBehaviour
     [SerializeField] private float damage;
     [SerializeField] private float attackCoolDown;
 
-    void Start()
+    private float timer = 0;
+    private bool enemyCanAttack = true;
+
+    private void FixedUpdate()
     {
-        
+        //Timer
+        if (enemyCanAttack == false)
+        {
+            timer += Time.deltaTime;
+            if (timer >= attackCoolDown)
+            {
+                enemyCanAttack = true;
+            }
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(tag == "Player")
+        {
+            MeleeHitPlayer(collision.gameObject.GetComponent<HealthManager>());
+        }
     }
 
-    void Update()
+    void MeleeHitPlayer(HealthManager health)
     {
-        
+        health.onLostHealth.Invoke(damage);
     }
 }
