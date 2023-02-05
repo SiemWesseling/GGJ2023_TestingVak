b474438c9955dcca6d8a1e13e3834b157bfc0fdf
Siem Wesseling <siemwesseling@gmail.com>
2023-02-05 01:21:59 +0100

    added enemy spawning
    enemies also respawn based on timer or amount of enemies left

--- Assets/Scripts/Characters/Enemy/EnemySpawn.cs
+++ Assets/Scripts/Characters/Enemy/EnemySpawn.cs
@@ -7,13 +7,18 @@ public class EnemySpawn : MonoBehaviour
     [Range(0, 100)]
     [SerializeField] private float distanceFromPlayer;
 
-    [SerializeField] private GameObject Enemy = new GameObject();
+    [SerializeField] private GameObject[] Enemy;
+
+    [SerializeField] private EnemyWavesSpawner enemyWavesSpawner;
+ 
+    private GameObject SpawnedEnemy;
 
     public void SpawnEnemy()
     {
+        SpawnedEnemy = Enemy[Random.Range(0, Enemy.Length)];
         Vector2 Distance = new Vector2(Random.Range(-1, 1), Random.Range(-1, 1)).normalized * distanceFromPlayer;
         Vector2 SpawnPosition = GameManager.Instance.player.transform.position;
         SpawnPosition += Distance;
-        Instantiate(Enemy, SpawnPosition, Quaternion.identity);
+        Instantiate(SpawnedEnemy, SpawnPosition, Quaternion.identity, enemyWavesSpawner.transform.parent);
     }
 }
--- Assets/Scripts/Characters/Enemy/EnemyWavesSpawner.cs
+++ Assets/Scripts/Characters/Enemy/EnemyWavesSpawner.cs
@@ -4,38 +4,67 @@ using UnityEngine;
 
 public class EnemyWavesSpawner : MonoBehaviour
 {
-    [SerializeField] private Wave wave;
+    //Todo: every enemy spawns as a child of enemy parent to be able to count enemies
+    //Todo: if x amount of children left, start timer2
+    //Todo: if children left smaller than x or timer <= 0, then start spawning next wave of enemies based on timer1
 
-    EnemySpawn enemySpawn;
-    private void Update()
+    [SerializeField] private int amountOfEnemiesPerWave;
+
+    [SerializeField] private GameObject EnemySpawner;
+
+    [Range(0, 100)]
+    [SerializeField] private float waveTimer;
+    private float waveTimeSeconds;
+
+    [Range(0, 100)]
+    [SerializeField] private int amountOfEnemiesLeftBeforeNewSpawn;
+
+    [Range(0, 1)]
+    [SerializeField] private float spawnTimer;
+
+    private EnemySpawn enemySpawn;
+
+    private bool coRoutineHasStarted;
+
+    private void Start()
     {
-        foreach 
+        enemySpawn = GetComponent<EnemySpawn>();
+
+        waveTimeSeconds = waveTimer;
+
+        StartCoroutine(SpawnEnemies());
     }
 
-    //Represents a single enemy spawn wave
-    [System.Serializable]
-    private class Wave
+    public void Update()
     {
-        [SerializeField] private EnemySpawn[] enemySpawnArray;
-        [SerializeField] private float timer;
-        
-        public void Update()
+        if (waveTimer >= 0)
         {
-            if (timer >= 0)
+            if (EnemySpawner.transform.childCount > amountOfEnemiesLeftBeforeNewSpawn)
             {
-                timer -= Time.deltaTime;
-                if (timer < 0)
+                waveTimer -= Time.deltaTime;
+            }
+            if (waveTimer < 0 || EnemySpawner.transform.childCount <= amountOfEnemiesLeftBeforeNewSpawn)
+            {
+                if (!coRoutineHasStarted)
                 {
-                    SpawnEnemies();
+                    StartCoroutine(SpawnEnemies());
                 }
+
+                waveTimer = waveTimeSeconds;
             }
         }
-        private void SpawnEnemies()
+    }
+
+    private IEnumerator SpawnEnemies()
+    {
+        coRoutineHasStarted = true;
+
+        for(int i = 0; i < amountOfEnemiesPerWave; i++)
         {
-            foreach (EnemySpawn enemySpawn in enemySpawnArray)
-            {
-                enemySpawn.SpawnEnemy();
-            }
+            enemySpawn.SpawnEnemy();
+            yield return new WaitForSeconds(spawnTimer);
         }
+
+        coRoutineHasStarted = false;
     }
 }
